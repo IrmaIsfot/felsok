4b40062a7ae277dbc9450de2e62fd76dc3fc8791
IrmaIsfot <skrappost66@hotmail.com>
2023-03-16 15:03:45 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 input.onButtonPressed(Button.A, function () {
-    RingbitCar.steering_angle(RingbitCar.Direction_turn.right, 360)
+    RingbitCar.running_distance(RingbitCar.Direction_run.forward, 2)
 })
 input.onButtonPressed(Button.B, function () {
-    RingbitCar.steering_angle(RingbitCar.Direction_turn.left, 360)
+	
 })
 RingbitCar.init_wheel(AnalogPin.P1, AnalogPin.P2)
 led.plot(randint(0, 4), randint(0, 4))
